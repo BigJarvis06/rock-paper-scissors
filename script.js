2b2ddd08e0e9852c4ffc771b7707bd2d15557abc
BigJarvis06 <tjdunn06@gmail.com>
2024-12-14 00:37:53 +1100

    added psuedocode for user and computer choice to script

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
+/* create function to generate a number between 1 - 3 and return a string value of either rock paper or scissors named getComputerChoice */
 
+/* create function to prompt the user to input an option of rock paper or scissors and return a number value between 1 - 3 */
