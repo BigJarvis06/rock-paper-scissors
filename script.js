4e8b846ab889ddd5bb9894c35bcce3d641513f4b
BigJarvis06 <tjdunn06@gmail.com>
2024-12-14 01:00:27 +1100

    created function getComputerChoice: returns string value of rock, paper or scissors randomly

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-/* create function to generate a number between 1 - 3 and return a string value of either rock paper or scissors named getComputerChoice */
-
-/* create function to prompt the user to input an option of rock paper or scissors and return a number value between 1 - 3 */
+/* create function  named getComputerChoice to generate a number between 1 - 3 and return a string value of either rock paper or scissors */
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
+    if (computerChoice === 1) {
+computerChoice = "Rock";
+    } else if (computerChoice === 2) {
+computerChoice = "Paper";
+    } else {
+computerChoice = "Scissors";
+    }
+    return computerChoice;
+}
+/* create function named getUserChoice to prompt the user to input an option of rock paper or scissors and return a number value between 1 - 3 */
