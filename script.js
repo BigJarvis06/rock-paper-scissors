4ac2ede7de6aef0423ad339bbfebe368a4d5870b
BigJarvis06 <tjdunn06@gmail.com>
2024-12-14 22:16:32 +1100

    added playRound function using switch statements

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let humanScore = 0;
 let computerScore = 0;
 /* create function  named getComputerChoice to generate a number between 1 - 3 and return a string value of either rock paper or scissors */
+let computerChoice;
 function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3) + 1;
+    computerChoice = Math.floor(Math.random() * 3) + 1;
     if (computerChoice === 1) {
 computerChoice = "Rock";
     } else if (computerChoice === 2) {
@@ -13,8 +14,21 @@ computerChoice = "Scissors";
     return computerChoice;
 }
 /* create function named getUserChoice to prompt the user to input an option of rock paper or scissors and return a number value between 1 - 3 */
+let humanChoice;
 function getHumanChoice() {
-let humanChoice = prompt().toLowerCase();
+humanChoice = prompt().toLowerCase();
+switch (humanChoice) {
+    case "rock":
+    case "paper":
+    case "scissors":
+        case "roc":
+            case "pape":
+            case "scissor":
+                return;
+                break;
+                default:
+                    alert("Input invalid")
 
 }
-/* create function to compare humanChoice and Computer choice and delcare a round winner*/
+}
+/* create function to compare humanChoice and computerChoice and declare a round winner*/
