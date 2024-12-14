6dec2497d4aac3b06307e2b4109ce582525c3512
BigJarvis06 <tjdunn06@gmail.com>
2024-12-15 00:23:16 +1100

    finished this headache

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ let computerChoice;
 function getComputerChoice() {
     computerChoice = Math.floor(Math.random() * 3) + 1;
     if (computerChoice === 1) {
-computerChoice = "Rock";
+computerChoice = "rock";
     } else if (computerChoice === 2) {
-computerChoice = "Paper";
+computerChoice = "paper";
     } else {
-computerChoice = "Scissors";
+computerChoice = "scissors";
     }
     return computerChoice;
 }
@@ -22,13 +22,68 @@ switch (humanChoice) {
     case "paper":
     case "scissors":
         case "roc":
+            humanChoice = "rock";
+            break;
             case "pape":
+                humanChoice = "paper";
+            break;
             case "scissor":
-                return;
-                break;
-                default:
-                    alert("Input invalid")
-
+                humanChoice = "scissors";
+            break;
 }
 }
 /* create function to compare humanChoice and computerChoice and declare a round winner*/
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+function playRound(computerChoice, humanChoice) {
+    switch (computerChoice) {
+        case "rock":
+            switch (humanChoice) {
+                case "rock":
+                alert("The round is a draw.");
+                break;
+                case "paper":
+                alert("You win! Paper beats rock.");
+                ++humanScore;
+                break;
+                case "scissors":
+                alert("You lose! Rock beats scissors.");
+                ++computerScore;
+                break;
+                default:
+                    alert("Input invalid.");
+            }
+        case "paper":
+            switch (humanChoice) {
+                case "rock":
+                alert("You lose! Paper beats rock.");
+                ++computerScore;
+                break;
+                case "paper":
+                alert("The round is a draw.");
+                break;
+                case "scissors":
+                alert("You win! Scissors beat paper.");
+                ++humanScore;
+                break;
+                default:
+                    alert("Input invalid.");
+            }
+        case "scissors":
+            switch (humanChoice) {
+                case "rock":
+                alert("You win! Rock beats scissors.");
+                ++humanScore;
+                break;
+                case "paper":
+                alert("You lose! Scissors beat paper");
+                ++computerScore;
+                break;
+                case "scissors":
+                alert("The round is a draw.");
+                break;
+                default:
+                    alert("Input invalid.");
+            }
+    }
+}
