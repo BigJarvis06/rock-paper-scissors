c0b695755980dbe1547222e837462ad6a35756ac
BigJarvis06 <tjdunn06@gmail.com>
2024-12-15 00:29:34 +1100

    fixed invalid user entry comparison

--- script.js
+++ script.js
@@ -1,89 +1,56 @@
 let humanScore = 0;
 let computerScore = 0;
 /* create function  named getComputerChoice to generate a number between 1 - 3 and return a string value of either rock paper or scissors */
-let computerChoice;
 function getComputerChoice() {
-    computerChoice = Math.floor(Math.random() * 3) + 1;
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
     if (computerChoice === 1) {
 computerChoice = "rock";
+return computerChoice;
     } else if (computerChoice === 2) {
 computerChoice = "paper";
+return computerChoice;
     } else {
 computerChoice = "scissors";
+return computerChoice;
     }
-    return computerChoice;
 }
 /* create function named getUserChoice to prompt the user to input an option of rock paper or scissors and return a number value between 1 - 3 */
-let humanChoice;
 function getHumanChoice() {
-humanChoice = prompt().toLowerCase();
-switch (humanChoice) {
-    case "rock":
-    case "paper":
-    case "scissors":
-        case "roc":
-            humanChoice = "rock";
-            break;
-            case "pape":
-                humanChoice = "paper";
-            break;
-            case "scissor":
-                humanChoice = "scissors";
-            break;
+    while (true) {
+let humanChoice = prompt("Enter rock, paper or scissors.").toLowerCase();
+if ((humanChoice === "rock") || (humanChoice === "paper") || (humanChoice == "scissors")) {
+    return humanChoice;
+} else {alert("Invalid entry")}
+} 
 }
-}
+
 /* create function to compare humanChoice and computerChoice and declare a round winner*/
-const computerSelection = getComputerChoice();
-const humanSelection = getHumanChoice();
 function playRound(computerChoice, humanChoice) {
-    switch (computerChoice) {
-        case "rock":
-            switch (humanChoice) {
-                case "rock":
-                alert("The round is a draw.");
-                break;
-                case "paper":
-                alert("You win! Paper beats rock.");
-                ++humanScore;
-                break;
-                case "scissors":
-                alert("You lose! Rock beats scissors.");
-                ++computerScore;
-                break;
-                default:
-                    alert("Input invalid.");
-            }
-        case "paper":
-            switch (humanChoice) {
-                case "rock":
-                alert("You lose! Paper beats rock.");
-                ++computerScore;
-                break;
-                case "paper":
-                alert("The round is a draw.");
-                break;
-                case "scissors":
-                alert("You win! Scissors beat paper.");
-                ++humanScore;
-                break;
-                default:
-                    alert("Input invalid.");
-            }
-        case "scissors":
-            switch (humanChoice) {
-                case "rock":
-                alert("You win! Rock beats scissors.");
-                ++humanScore;
-                break;
-                case "paper":
-                alert("You lose! Scissors beat paper");
-                ++computerScore;
-                break;
-                case "scissors":
-                alert("The round is a draw.");
-                break;
-                default:
-                    alert("Input invalid.");
-            }
+   if (computerChoice === humanChoice) {
+    alert("The round is a draw.");
+   } else if (
+    (computerChoice === "rock" && humanChoice === "paper") || 
+          (computerChoice === "paper" && humanChoice === "scissors") || 
+           (computerChoice === "scissors" && humanChoice === "rock")
+        ) {
+           alert(`You win! ${humanChoice} beats ${computerChoice}.`)
+           humanScore++
+    } else {
+           alert(`You lose! ${computerChoice} beats ${humanChoice}.`)
+           computerScore++
+    } 
+    console.log(`Computer score ${computerScore}. Your score ${humanScore}.`);
+}
+function playGame() {
+    while ((computerScore < 3) && (humanScore < 3)) {
+        const computerSelection = getComputerChoice();
+        const humanSelection = getHumanChoice();
+        playRound(computerSelection, humanSelection);
+    }
+    if (humanScore === 3) {
+        alert("You win!");
+    } else {
+        alert("You lose!");
     }
 }
+  
